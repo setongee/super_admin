5da79ca4f7aab29e80f9cfb8543798b047393d48
setongee <setongee@gmail.com>
2024-08-29 14:42:11 +0100

    fixe4d digital ocean

--- src/api/environment.js
+++ src/api/environment.js
@@ -1,4 +1,4 @@
-const environment = { test : "http://localhost:8000/api/v2", live : "https://lasgbackendv2.onrender.com/api/v2" }
+const environment = { test : "http://localhost:8000/api/v2", live : "https://oyster-app-7x7p7.ondigitalocean.app/api/v2" }
 
 const env = environment.live
 
