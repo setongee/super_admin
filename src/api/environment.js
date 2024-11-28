b43e478cd8c0abd6e2454224417c2458a5ea4b6d
setongee <setongee@gmail.com>
2024-11-28 21:04:35 +0100

    new way

--- src/api/environment.js
+++ src/api/environment.js
@@ -1,5 +1,5 @@
 const environment = { test : "http://localhost:8000/api/v2", live : "https://oyster-app-7x7p7.ondigitalocean.app/api/v2" }
 
-const env = environment.test
+const env = environment.live
 
 export { env }
