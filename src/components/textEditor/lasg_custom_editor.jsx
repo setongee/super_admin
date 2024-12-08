8d1b5d74be8a5d40ede2e4ae8e42f185f7057e00
setongee <setongee@gmail.com>
2024-12-08 23:40:09 +0100

    fixed content photo

--- src/components/textEditor/lasg_custom_editor.jsx
+++ src/components/textEditor/lasg_custom_editor.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Editor } from 'primereact/editor';        
 import "primereact/resources/themes/lara-light-teal/theme.css";
+import './editor.scss'
 
 export default function LASGEditor({value, readOnly, submittableText}) {
 
