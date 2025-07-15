1a1ffcd50277f5b3a1ae0483d5d015bee4e11e53
setongee <setongee@gmail.com>
2025-07-15 21:06:58 +0100

    push api https 2

--- src/api/environment.js
+++ src/api/environment.js
@@ -1,4 +1,4 @@
-const environment = { test : "http://localhost:8000/api/v2", live : "http://web3.lagosstate.gov.ng:8083/api/v2" }
+const environment = { test : "http://localhost:8000/api/v2", live : "https://web3.lagosstate.gov.ng/api/v2" }
 
 const env = environment.live
 
