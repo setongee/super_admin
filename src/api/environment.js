3de16994cadd484ae4a7b82f29860403e6e6a034
setongee <setongee@gmail.com>
2025-07-15 18:21:03 +0100

    fixed api route

--- src/api/environment.js
+++ src/api/environment.js
@@ -1,4 +1,4 @@
-const environment = { test : "http://localhost:8000/api/v2", live : "https://oyster-app-7x7p7.ondigitalocean.app/api/v2" }
+const environment = { test : "http://localhost:8000/api/v2", live : "http://web3.lagosstate.gov.ng:8083/api/v2" }
 
 const env = environment.live
 
