de4d8f017f9e79a0e423b6d91f20cf0df8099dd2
setongee <setongee@gmail.com>
2026-03-09 08:37:44 +0100

    changed to live

--- src/api/environment.js
+++ src/api/environment.js
@@ -1,5 +1,8 @@
-const environment = { test : "http://localhost:8083/api/v2", live : "https://web3.lagosstate.gov.ng/api/v2" }
+const environment = {
+  test: "http://localhost:8083/api/v2",
+  live: "https://web3.lagosstate.gov.ng/api/v2",
+};
 
-const env = environment.test
+const env = environment.live;
 
-export { env }
+export { env };
